1e91ae63f98b7a9308cfb17a98615f25d44296f1
Camila Ramos <cpr.fisica@gmail.com>
2025-10-28 16:54:19 -0300

    small fixes on script for MA5

--- ATLAS-EXOT-2018-06/getMA5results.py
+++ ATLAS-EXOT-2018-06/getMA5results.py
@@ -8,27 +8,62 @@ import numpy as np
 import xml.etree.ElementTree as ET
 
 # SAF and CLs file patterns
-cutflow_patterns = ['/home/ramos/madanalysis5-1_11_0/ANALYSIS_17/Output/SAF/dmp_1_350_1j_match/atlas_exot_2018_06/Cutflows/*.saf']
-saf_patterns = ['/home/ramos/madanalysis5-1_11_0/ANALYSIS_17/Output/SAF/*/*.saf']
-cls_patterns = ['/home/ramos/madanalysis5-1_11_0/ANALYSIS_17/Output/SAF/*/CLs_output.dat']
-inputList_pattern = sorted(glob.glob('/home/ramos/madanalysis5-1_11_0/ANALYSIS_17/Input/*.list'))
+saf_patterns = ['/home/ramos/MonoXSMS/ATLAS-EXOT-2018-06/ma5_axial_all_add/Output/SAF/*/*.saf']
+cls_patterns = ['/home/ramos/MonoXSMS/ATLAS-EXOT-2018-06/ma5_axial_all_add/Output/SAF/*/CLs_output.dat']
+inputList_pattern = sorted(glob.glob('/home/ramos/MonoXSMS/ATLAS-EXOT-2018-06/ma5_axial_all_add/Input/*.list'))
+
 
 saf_to_theoretical = {}
 final_results = []
 allDataDicts = []
 
-cutflow_files = sorted([file for pattern in cutflow_patterns for file in glob.glob(pattern)])
 saf_files = sorted([file for pattern in saf_patterns for file in glob.glob(pattern)])
 cls_files = sorted([file for pattern in cls_patterns for file in glob.glob(pattern)])
-
 
 if len(inputList_pattern) != len(cls_files):
     print(f"Warning: Mismatch in input files ({len(inputList_pattern)}) and CLs files ({len(cls_files)})!")
 
 sample_global_info_pattern = re.compile(r"<SampleGlobalInfo>(.*?)</SampleGlobalInfo>", re.DOTALL)
 
+def extract_all_cuts_weights(em_file_path):
+    with open(em_file_path, 'r') as file:
+        lines = file.readlines()
+    weights = {}
+    weights_sq = {}
+    for i, line in enumerate(lines):
+        if line.startswith('<Counter>') or line.startswith('<InitialCounter>'):
+            cut_name = lines[i + 1].strip().split('"')[1]
+            if 'sum of weights' and not '^2' in lines[i + 2]:
+                val = float(lines[i + 3].split()[0])
+                valsq = float(lines[i + 4].split()[0])
+                weights[cut_name] = val
+                weights_sq[cut_name] = valsq
+    return weights, weights_sq
+
+# Define the desired cuts
+desired_cuts = [
+    'Initial number of events','MET > 200 GeV(IM0)', 'MET > 250 GeV(IM1)', 
+    'MET > 300 GeV(IM2)', 'MET > 350 GeV(IM3)', 'MET > 400 GeV(IM4)', 
+    'MET > 500 GeV(IM5)', 'MET > 600 GeV(IM6)', 'MET > 700 GeV(IM7)', 
+    'MET > 800 GeV(IM8)', 'MET > 900 GeV(IM9)', 'MET > 1000 GeV(IM10)', 
+    'MET > 1100 GeV(IM11)', 'MET > 1200 GeV(IM12)','200 < MET < 250 GeV', 
+    '250 < MET < 300 GeV', '300 < MET < 350 GeV', '350 < MET < 400 GeV', 
+    '400 < MET < 500 GeV', '500 < MET < 600 GeV', '600 < MET < 700 GeV', 
+    '700 < MET < 800 GeV', '800 < MET < 900 GeV', '900 < MET < 1000 GeV', 
+    '1000 < MET < 1100 GeV', '1100 < MET < 1200 GeV', 'MET < 1200 GeV'
+]
+
+sr_list = ['IM0', 'IM1', 'IM2', 'IM3', 'IM4', 'IM5', 'IM6', 'IM7', 'IM8', 'IM9', 'IM10', 'IM11', 'IM12',
+           'EM0', 'EM1', 'EM2', 'EM3', 'EM4', 'EM5', 'EM6', 'EM7', 'EM8', 'EM9', 'EM10', 'EM11', 'EM12']
+
 # Process SAF and CLs files in pairs
+
+
 for inputFile, cls_file, saf_file in zip(inputList_pattern, cls_files, saf_files):
+
+    cutflow_pattern = os.path.dirname(saf_file)+'/atlas_exot_2018_06/Cutflows/*.saf'
+    cutflow_files = sorted([file for file in glob.glob(cutflow_pattern)])
+
     try:
         # Read the HepMC file location from the input file
         with open(inputFile, 'r') as f:
@@ -39,9 +74,7 @@ for inputFile, cls_file, saf_file in zip(inputList_pattern, cls_files, saf_files
             print(f"Cannot obtain banner, no hepmc file in Input.")
             continue
 
-        
         hepDir = os.path.dirname(hepMCfile_list[0])
-        print(hepDir)
         banner_candidates = glob.glob(f"{hepDir}/**/*banner.txt", recursive=True)
         
         if not banner_candidates:
@@ -72,6 +105,46 @@ for inputFile, cls_file, saf_file in zip(inputList_pattern, cls_files, saf_files
             "mMed (GeV)": mMed,
             "mchi (GeV)": mchi
         })
+
+        # Get efficiency and statistical error
+        w_dict, w2_dict = {}, {}
+
+        for cutflow_file in cutflow_files:
+            w_dict[cutflow_file], w2_dict[cutflow_file] = extract_all_cuts_weights(cutflow_file)
+
+        # Notify if no files were found for the current base path
+        if not cutflow_files:
+            print(f"No input files found for base path: {cutflow_patterns}")
+
+
+        # Create the nentries array with relevant cuts
+        weights = [0] * len(desired_cuts)
+        weights_sq = [0] * len(desired_cuts)
+
+        for cuts in w_dict.values():
+            for i, cut in enumerate(desired_cuts):
+                if cut in cuts:
+                    weights[i] = cuts[cut]
+
+        for cuts in w2_dict.values():
+            for i, cut in enumerate(desired_cuts):
+                if cut in cuts:
+                    weights_sq[i] = cuts[cut]
+
+
+        n_err = np.sqrt(np.array(weights_sq[1:]))
+        n_tot = weights[0]
+        effs = np.array(weights[1:])/n_tot
+        # effErr = (nerr / nevts) * eff
+        effs_stat = (n_err / n_tot) * effs
+
+        effs_dict = {}
+        effs_stat_dict = {}
+
+        for i, sr_val in enumerate(sr_list):
+            effs_dict[sr_val] = effs[i]
+            effs_stat_dict[sr_val] = effs_stat[i]
+
 
         # theoretical cross-section from the SAF file
         sigma_theoretical = None
@@ -120,8 +193,8 @@ for inputFile, cls_file, saf_file in zip(inputList_pattern, cls_files, saf_files
                     # efficiency = float(efficiency_parts[0])  # Efficiency
                     # stat = float(efficiency_parts[1])  # Statistical uncertainty
                     final_results.append((sr, sigma_exp, sigma_obs,
-                                          sigma_theoretical, os.path.basename(saf_file).replace('.saf', ''),
-                                          mMed, mchi))
+                                        sigma_theoretical, os.path.basename(saf_file).replace('.saf', ''),
+                                        mMed, mchi, effs_dict[sr], effs_stat_dict[sr]))
                 except (ValueError, IndexError) as e:
                     print(f"Error processing line: {line.strip()}\n{e}")
 
@@ -132,78 +205,13 @@ for inputFile, cls_file, saf_file in zip(inputList_pattern, cls_files, saf_files
     except Exception as e:
         print(f"Unexpected error processing {inputFile}: {e}")
 
+
 df_data = pd.DataFrame(allDataDicts)
 
 df_final_results = pd.DataFrame(
     final_results,
      columns=["Signal Region", "Sigma_95_exp (pb)", "Sigma_95_obs (pb)",
-             "Sigma_theoretical (pb)", "SAF File", "mMed (GeV)", "mchi (GeV)"])
-
-# Get efficiencies and error
-
-def extract_all_cuts_weights(em_file_path):
-    with open(em_file_path, 'r') as file:
-        lines = file.readlines()
-    weights = {}
-    weights_sq = {}
-    for i, line in enumerate(lines):
-        if line.startswith('<Counter>') or line.startswith('<InitialCounter>'):
-            cut_name = lines[i + 1].strip().split('"')[1]
-            if 'sum of weights' and not '^2' in lines[i + 2]:
-                val = float(lines[i + 3].split()[0])
-                valsq = float(lines[i + 4].split()[0])
-                weights[cut_name] = val
-                weights_sq[cut_name] = valsq
-    return weights, weights_sq
-
-# Define the desired cuts
-desired_cuts = [
-    'Initial number of events','MET > 200 GeV(IM0)', 'MET > 250 GeV(IM1)', 
-    'MET > 300 GeV(IM2)', 'MET > 350 GeV(IM3)', 'MET > 400 GeV(IM4)', 
-    'MET > 500 GeV(IM5)', 'MET > 600 GeV(IM6)', 'MET > 700 GeV(IM7)', 
-    'MET > 800 GeV(IM8)', 'MET > 900 GeV(IM9)', 'MET > 1000 GeV(IM10)', 
-    'MET > 1100 GeV(IM11)', 'MET > 1200 GeV(IM12)','200 < MET < 250 GeV', 
-    '250 < MET < 300 GeV', '300 < MET < 350 GeV', '350 < MET < 400 GeV', 
-    '400 < MET < 500 GeV', '500 < MET < 600 GeV', '600 < MET < 700 GeV', 
-    '700 < MET < 800 GeV', '800 < MET < 900 GeV', '900 < MET < 1000 GeV', 
-    '1000 < MET < 1100 GeV', '1100 < MET < 1200 GeV', 'MET < 1200 GeV'
-]
+             "Sigma_theoretical (pb)", "SAF File", "mMed (GeV)", "mchi (GeV)", "Efficiency", "Efficiency_Err"])
 
 
-# Notify if no files were found for the current base path
-if not cutflow_files:
-    print(f"No input files found for base path: {cutflow_patterns}")
-   
-
-w_dict, w2_dict = {}, {}
-
-for em_file in cutflow_files:
-    w_dict[em_file], w2_dict[em_file] = extract_all_cuts_weights(em_file)
-
-
-# Create the nentries array with relevant cuts
-weights = [0] * len(desired_cuts)
-weights_sq = [0] * len(desired_cuts)
-
-for cuts in w_dict.values():
-    for i, cut in enumerate(desired_cuts):
-        if cut in cuts:
-            weights[i] = cuts[cut]
-
-for cuts in w2_dict.values():
-    for i, cut in enumerate(desired_cuts):
-        if cut in cuts:
-            weights_sq[i] = cuts[cut]
-
-
-n_err = np.sqrt(np.array(weights_sq[1:]))
-n_tot = weights[0]
-effs = np.array(weights[1:])/n_tot
-# effErr = (nerr / nevts) * eff
-effs_stat = (n_err / n_tot) * effs
-
-df_final_results['Efficiency'] = effs
-df_final_results['Efficiency_Err'] = effs_stat
-
-
-df_final_results.to_pickle('/home/ramos/MonoXSMS-camila/ATLAS-EXOT-2018-06/ma5_test_spin1/ma5Results_axial.pcl')
+df_final_results.to_pickle('/home/ramos/MonoXSMS/ATLAS-EXOT-2018-06/ma5_axial_all_add/ma5Results_axial_add_effs-correct.pcl')
---
+++ ATLAS-EXOT-2018-06/runMA5-multi.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+
+# Run MadAnalysis5 for already generated hepmc files
+
+import sys,os,glob
+import pyslha
+import xml.etree.ElementTree as ET
+import subprocess, logging, shutil
+import multiprocessing
+import tempfile
+import time, datetime
+from configParserWrapper import ConfigParserExt
+
+FORMAT = '%(levelname)s in %(module)s.%(funcName)s(): %(message)s at %(asctime)s'
+logging.basicConfig(format=FORMAT,datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger("MA5Scan")
+
+logger.info("Hello!")
+
+
+def getDataFromBanner(banner):
+    with open(banner, 'r') as f:
+        lines = f.readlines()
+        
+    isxsecBlock = False
+    xsec = None
+    xsecBlock = []
+    nEvents = None  
+
+    for i, line in enumerate(lines):
+        if '<MGGenerationInfo>' in line:
+            isxsecBlock = True
+            continue
+        elif '</MGGenerationInfo>' in line:
+            isxsecBlock = False
+            continue
+        if isxsecBlock:
+            xsecBlock.append(line)
+        # if 'Number of events' in line:  
+        #     try:
+        #         nEvents = int(line.split(':')[-1].strip())
+        #     except ValueError:
+        #         pass
+
+    nevts_str = xsecBlock[0].strip('\n').split(' ')[-1]
+    nEvents = float(nevts_str)
+    xsec_str = xsecBlock[-1].strip('\n').split(' ')[-1]
+    xsec = float(xsec_str)
+    print(nEvents, xsec)
+
+    xtree = ET.parse(banner)
+    xroot = xtree.getroot()
+    slha = xroot.find('header').find('slha').text
+    pars = pyslha.readSLHA(slha)
+
+    if 55 in pars.blocks['MASS']:
+        model = 'spin1'
+        mMed = pars.blocks['MASS'][55]
+    elif 54 in pars.blocks['MASS']:
+        model = 'spin0'
+        mMed = pars.blocks['MASS'][54]
+    elif 2000002 in pars.blocks['MASS']:
+        model = 'tchannel'
+        mMed = pars.blocks['MASS'][2000002]
+
+    if model in ['spin1','spin0']:
+        mDM = pars.blocks['MASS'][52]
+    elif model in ['tchannel']:
+        mDM = pars.blocks['MASS'][57]
+    else:
+        mDM = None
+
+    if model == 'spin1':
+        gVq = pars.blocks['DMINPUTS'][4]
+        gAq = pars.blocks['DMINPUTS'][10]
+        gVx = pars.blocks['DMINPUTS'][2]
+        gAx = pars.blocks['DMINPUTS'][3]
+        if (gAq != 0) and (gAx != 0):
+            ma5FolderName = f'dma_{int(mDM)}_{int(mMed)}'
+        else:
+            ma5FolderName = f'dmv_{int(mDM)}_{int(mMed)}'
+
+    elif model == 'spin0':
+        gVq = pars.blocks['DMINPUTS'][6]
+        gAq = pars.blocks['DMINPUTS'][12]
+        gVx = pars.blocks['DMINPUTS'][3]
+        gAx = pars.blocks['DMINPUTS'][4]
+        if (gAq != 0) and (gAx != 0):
+            ma5FolderName = f'dmp_{int(mDM)}_{int(mMed)}'
+        else:
+            ma5FolderName = f'dms_{int(mDM)}_{int(mMed)}'
+
+    elif model == 'tchannel':
+        lam = pars.blocks['DMS3U'][1,1]
+        ma5FolderName = f'dmys3u1_{int(mDM)}_{int(mMed)}'
+
+    return xsec, nEvents, ma5FolderName  
+
+
+
+def createCommandsFile(parser):
+    """
+    Creates the command file for MadAnalysis5 based on parameters passed by user.
+    Supports multiple MG5 folders and multiple runs per benchmark.
+    Automatically merges samples with identical benchmark names.
+    """
+    pars = parser['MA5Pars']
+    sets = parser['MA5Set']
+    ma5Options = parser['options']
+    anaFolder = pars['anaFolder']
+
+    mg5Folders = pars['mg5Folder']
+    if isinstance(mg5Folders, str):
+        mg5Folders = [f.strip() for f in mg5Folders.replace(',', ' ').split()]
+
+    # Structure: benchmark → mg5Folder → list of (xsec, nEvents, hepmc)
+    data = {}
+
+    for mg5Folder in mg5Folders:
+        mg5Folder = os.path.abspath(mg5Folder)
+        banners = sorted(glob.glob(os.path.join(mg5Folder, 'Events/run_*/') + '*banner.txt'))
+        hempcFiles = sorted(glob.glob(os.path.join(mg5Folder, 'Events/run_*/') + '*hepmc.gz'))
+        
+        for banner, hempc in zip(banners, hempcFiles):
+            try:
+                xsec, nEvents, folderName = getDataFromBanner(banner)
+            except Exception as e:
+                logger.error(f"Failed to parse {banner}: {e}")
+                continue
+            if folderName not in data:
+                data[folderName] = {}
+            if mg5Folder not in data[folderName]:
+                data[folderName][mg5Folder] = []
+            data[folderName][mg5Folder].append((xsec, nEvents, hempc))
+
+    commandsFile = pars['commandsFile']
+    storeRoot = ma5Options['storeRoot']
+    ma5CardPath = pars['ma5CardPath']
+
+    with open(commandsFile, 'w') as f:
+        f.write('set main.recast = on\n')
+        f.write(f'set main.recast.store_root = {storeRoot}\n')
+        f.write(f'set main.recast.card_path = {ma5CardPath}\n')
+        if 'lumi' in sets:
+            f.write(f'set main.recast.add.extrapolated_luminosity = {sets["lumi"]}\n')
+
+        for folderName, folderData in data.items():
+            total_xsec = 0
+            all_files = []
+            for mg5Folder, runs in folderData.items():
+                total_events = sum(n for _, n, _ in runs)
+                if total_events == 0:
+                    continue
+                weighted_xsec = sum(x * n for x, n, _ in runs) / total_events
+                total_xsec += weighted_xsec
+                all_files.extend(h for _, _, h in runs)
+
+            f.write(f'# === {folderName} ===\n')
+            for hempc in all_files:
+                f.write(f'import {hempc} as {folderName}\n')
+            f.write(f'set {folderName}.xsection = {total_xsec}\n')
+            logger.info(f'{folderName}: total xsec = {total_xsec:.6f} pb ({len(all_files)} files)\n')
+
+        f.write(f'submit {anaFolder}\n')
+
+    return commandsFile, list(data.keys())[-1] if data else None
+
+def runMA5(parser):
+
+    t0 = time.time()
+
+    commandsFile, folderName = createCommandsFile(parser)
+
+    pars = parser['MA5Pars']
+
+    logger.info('Generating process using %s' %commandsFile)
+
+    #Generate process
+    # ma5Folder = os.path.abspath('/home/ramos/madanalysis5-1_11_0')
+    # run = subprocess.Popen('./bin/ma5 -R -s %s' %commandsFile,shell=True,
+    #                             stdout=subprocess.PIPE,stderr=subprocess.PIPE,
+    #                             cwd=ma5Folder)
+
+    # output,errorMsg = run.communicate()
+    # logger.debug('MA5 process error:\n %s \n' %errorMsg)
+    # logger.debug('MA5 process output:\n %s \n' %output)
+    # logger.info('Finished MadAnalysis5 processes.')
+
+    runInfo = {'time (s)': time.time() - t0}
+    runInfo.update({'processFolder': folderName})
+    runInfo.update(pars)
+
+    return runInfo
+
+
+
+def main(parfile, verbose):
+    level = verbose
+    levels = {'debug': logging.DEBUG,
+              'info': logging. INFO,
+              'warn': logging.WARNING,
+              'error': logging.ERROR}
+    
+    if not level in levels:
+        logger.error(f'Unknown log level "{level}" supplied.')
+        sys.exit()
+
+    logger.setLevel(level = levels[level])
+
+    parser = ConfigParserExt(inline_comment_prefixes='#')
+    ret = parser.read(parfile)
+
+    if ret == []:
+        logger.error(f'No such file or directory: "{args.parfile}"')
+
+    #Get a list of parsers (in case loops have been defined)    
+    parserList = parser.expandLoops()
+
+    now = datetime.datetime.now()
+    for irun, newParser in enumerate(parserList):
+        mg5FolderList = newParser.get('MA5Pars','mg5Folder')
+
+        for mg5Folder in mg5FolderList:
+            mg5Folder = os.path.abspath(mg5Folder)
+
+            if mg5Folder[-1] == '/':
+                mg5Folder = mg5Folder[:-1]
+            if not os.path.isdir(mg5Folder):
+                logger.error(f'Folder {mg5Folder} not found!')
+                sys.exit()
+
+        parserDict = newParser.toDict(raw=False)
+        logger.debug('submitting with pars:\n %s \n' %parserDict)
+        output = runMA5(parserDict)    
+
+#     Wait for jobs to finish:
+    logger.info("Finished analyzing all samples at %s" %(now.strftime("%Y-%m-%d %H:%M")))
+
+    return output
+
+if __name__ == '__main__':
+
+    import argparse
+    ap = argparse.ArgumentParser( description=
+            "Run a (serial) MadAnalysis5 scan for the parameters defined in the parameters file." )
+    ap.add_argument('-p', '--parfile', default='scan_parameters.ini',
+            help='path to the parameters file [scan_parameters.ini].')
+    ap.add_argument('-v', '--verbose', default='info',
+            help='verbose level (debug, info, warning or error). Default is info')
+
+
+    t0 = time.time()
+
+    args = ap.parse_args()
+    output = main(args.parfile,args.verbose)
+            
+    print("\n\nDone in %3.2f min" %((time.time()-t0)/60.))
+
+
+
+
+        
+        
